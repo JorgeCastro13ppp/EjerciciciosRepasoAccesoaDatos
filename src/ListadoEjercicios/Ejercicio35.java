5d0bd1c118497001f4f1145b6835025808f7bb3f
JorgeCastro13ppp <jlcastroc01@educarex.es>
2025-09-24 10:35:05 +0200

    ff

--- src/ListadoEjercicios/Ejercicio35.java
+++ src/ListadoEjercicios/Ejercicio35.java
@@ -11,7 +11,7 @@ public class Ejercicio35 {
         
      // Añadimos cinco pares clave-profesor
         claveProfesor.put("IFC02", "Juan Pérez");
-        claveProfesor.put("FOL01", "María López");
+        claveProfesor.put("FOL01", "María López");//
         claveProfesor.put("IFC08", "Carlos Ruiz");
         claveProfesor.put("ING06", "Ana Torres");
         claveProfesor.put("FPIFC01", "Laura Gómez");
